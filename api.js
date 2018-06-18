2eacb4fab0d77a5ded8cd5415eaaafc8aa6a7f4e
slobx <slobxns@gmail.com>
2018-06-18 19:44:09 +0200

    implemented save and delete contact

--- api.js
+++ api.js
@@ -3,22 +3,22 @@ let sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database(':memory:'); // database will be stored in RAM only
 
 db.serialize(function () {
-    db.run("CREATE TABLE IF NOT EXISTS phone_book (id INTEGER PRIMARY KEY, name TEXT, last_name TEXT, phone TEXT)");
-    db.run("INSERT INTO phone_book (name, last_name, phone) VALUES (?, ?, ?)", "Bob", "Washington", "+381641234567");
-    db.run("INSERT INTO phone_book (name, last_name, phone) VALUES (?, ?, ?)", "Alice", "Marlon", "+38163122333");
-    db.run("INSERT INTO phone_book (name, last_name, phone) VALUES (?, ?, ?)", "Lee", "Jackson", "+381628000111");
+    db.run("CREATE TABLE IF NOT EXISTS phone_book (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, last_name TEXT, phone TEXT)");
 });
 
 // define api object
 const express = require('express');
-const api = express();
-let bodyParser = require('body-parser');
-api.use(bodyParser.json());
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(cors);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // define API endpoints
 
 //get contact by ID
-api.get('/api:id', (req, res) => {
+app.get('/api:id', (req, res) => {
     const id = req.params['id'];
     db.get("SELECT * FROM phone_book WHERE id=(?)", id, function (err, row) {
         res.json({
@@ -31,34 +31,55 @@ api.get('/api:id', (req, res) => {
 
 
 //get all contacts
-api.get('/api/contacts.json', (req, res) => {
+app.get('/api/contacts_list', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     db.all("SELECT * FROM phone_book", function (err, row) {
-        res.json(row);
+        if (row) {
+            res.json(row);
+        } else {
+            return {}
+        }
     });
 });
 
-api.post('/api/post',  function (req, res) {
+//save contact
+app.post('/api/add_contact', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    let sql = 'INSERT INTO phone_book (name, last_name, phone) VALUES (?, ?, ?)';
+    let data = [
+        req.body.name,
+        req.body.last_name,
+        req.body.phone
+    ];
+
+    db.run(sql, data,
+        function (err) {
+            if (err) {
+                return console.error(err.message);
+            } else {
+                console.log(`Rows inserted`);
+            }
+        }
+    );
+});
+
+//delete contact
+app.delete('/api/delete_contact', function (req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     console.log(req.body);
-    // let sql = 'INSERT INTO phone_book (name, last_name, phone) VALUES (?, ?, ?)';
-    // let data = [
-    //     req.body.name,
-    //     req.body.last_name,
-    //     req.body.phone
-    // ];
-    //
-    // db.run(sql, data,
-    //     function(err) {
-    //         if (err) {
-    //             return console.error(err.message);
-    //         }
-    //         console.log(`Rows inserted`);
-    //     }
-    //     );
+
+    db.run(`DELETE FROM phone_book WHERE id=?`, req.body.id, function (err) {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log(`Row(s) deleted ${this.changes}`);
+    });
+
+
 });
 
 // run server
-    api.listen(3000, () => console.log('Listening on port 3000...'));
+app.listen(3000, () => console.log('Listening on port 3000...'));
